a41ce5976f931bacf39ccca822fc9c9e1fdae78b
Sychev Eugene <dev.stratus@gmail.com>
2024-06-16 22:50:21 +0300

    4 brane-game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playProgressionGame from '../src/games/brain-progression-game.js';
+
+playProgressionGame();
---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,26 @@
+import playGame from '../index.js';
+
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateProgression = (start, step, length) => {
+  return Array.from({ length }, (_, i) => start + i * step);
+};
+
+const generateRound = () => {
+  const start = generateRandomNumber(1, 10);
+  const step = generateRandomNumber(1, 5);
+  const length = generateRandomNumber(5, 10);
+  const progression = generateProgression(start, step, length);
+  const hiddenIndex = generateRandomNumber(0, length - 1);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+const gameDescription = 'What number is missing in the progression?';
+
+const playProgressionGame = () => playGame(gameDescription, generateRound);
+
+export default playProgressionGame;
